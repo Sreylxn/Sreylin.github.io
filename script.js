3119e655dab7c177244c932590538a2a864dd6ee
Sreylin Strong <sreylinstrong@MacBookAir.lan>
2025-07-26 00:26:40 +0100

    styled new page and added copy clipboard

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function toggleModules() {
     const modules = document.getElementById('display-modules');
     modules.classList.toggle('open');
 }
+
+
+function setupCopy(buttonId, textId, successMessage) {
+    const btn = document.getElementById(buttonId);
+    const text = document.getElementById(textId);
+
+    if (btn && text) {
+        btn.addEventListener("click", () => {
+            navigator.clipboard.writeText(text.innerText).then(() => {
+                alert(successMessage);
+            });
+        });
+    }
+}
+
+setupCopy("copyEmailBtn", "emailText", "Email copied!");
+setupCopy("copyPhoneBtn", "phoneText", "Phone number copied!");
